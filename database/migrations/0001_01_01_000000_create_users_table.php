430b9bfe22e44c78048a3cd5833fcae0615a0745
haurayatha <242510226@smkn4-tsm.sch.id>
2026-08-11 11:40:41 +0700

    divv

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+        if (DB::getDriverName() === 'sqlite') {
             $table->fullText(['name', 'email']);
+        }
             $table->rememberToken();
             $table->timestamps();
         });
